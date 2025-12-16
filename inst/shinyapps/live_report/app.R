4f97458bf1d0bedfa7457099219a5e32de659c12
ivinna <stich80@mail.ru>
2025-12-16 15:55:04 +0300

    Update app.R

--- inst/shinyapps/live_report/app.R
+++ inst/shinyapps/live_report/app.R
@@ -7,12 +7,11 @@ library(tidyr)
 
 # Устанавливаем локаль один раз при запуске
 Sys.setlocale("LC_TIME", "RUSSIAN")
-# load("joined_data_vars_trim.RData")
-# load("results_2025.RData")
+
 load("results_df.RData")
 
-results_2025 <- results_df %>% 
-  # select(Город, Дата,`Сп. Дисциплина`, comp_id, Событие, Пол) %>% 
+results_df <- results_df %>% 
+  select(-len, -n, -lens) %>%
   mutate(Title = paste(Дата, Город,`Сп. Дисциплина`, Событие, Пол)) %>% 
   mutate(split = str_replace_all(split, "Претайминг", ""))
   
@@ -22,22 +21,14 @@ text_filter <- function(x, y) {
   str_detect(x, regex(pattern, ignore_case = TRUE))
 }
 
-laps <- results_2025 %>% 
-  mutate(len = as.numeric(len), 
-         n = as.numeric(n)) %>% 
+laps <- results_df %>% 
   group_by(comp_id) %>% 
   slice(1) %>% 
   ungroup %>% 
   select(Город, Дата,`Сп. Дисциплина`, comp_id, Событие, Пол) %>% 
   mutate(Title = paste(Дата, Город,`Сп. Дисциплина`, Событие, Пол))
 
-find_laps = function(vec){
-  x = vec[1]
-  y = vec[2]
-  seq(from = 0, to = x, by = y)}
-
 diff_w_next <- function(x){c(x[1],(x[2:length(x)]-x[1:(length(x)-1)]))}
-
 
 # пример app.R
 
@@ -66,7 +57,7 @@ ui <- fluidPage(
       selectizeInput(
         inputId = "ath_1",
         label = "Выберите спортсмена:",
-        choices = c("Оставить пустым", sort(unique(results_2025$athlete))),
+        choices = c("Оставить пустым", sort(unique(results_df$athlete))),
       multiple = FALSE,
       options = list(placeholder = 'Начните вводить для поиска')
     )
@@ -77,7 +68,7 @@ ui <- fluidPage(
     selectizeInput(
       inputId = "ath_2",
       label = "Выберите спортсмена:",
-      choices = c("Оставить пустым", sort(unique(results_2025$athlete))),
+      choices = c("Оставить пустым", sort(unique(results_df$athlete))),
       multiple = FALSE,
       options = list(placeholder = 'Начните вводить для поиска')
     )
@@ -88,7 +79,7 @@ ui <- fluidPage(
     selectizeInput(
       inputId = "ath_3",
       label = "Выберите спортсмена:",
-      choices = c("Оставить пустым", sort(unique(results_2025$athlete))),
+      choices = c("Оставить пустым", sort(unique(results_df$athlete))),
       multiple = FALSE,
       options = list(placeholder = 'Начните вводить для поиска')
     )
@@ -99,7 +90,7 @@ ui <- fluidPage(
     selectizeInput(
       inputId = "ath_4",
       label = "Выберите спортсмена:",
-      choices = c("Оставить пустым", sort(unique(results_2025$athlete))),
+      choices = c("Оставить пустым", sort(unique(results_df$athlete))),
       multiple = FALSE,
       options = list(placeholder = 'Начните вводить для поиска')
     )
@@ -110,7 +101,7 @@ ui <- fluidPage(
     selectizeInput(
       inputId = "ath_5",
       label = "Выберите спортсмена:",
-      choices = c("Оставить пустым", sort(unique(results_2025$athlete))),
+      choices = c("Оставить пустым", sort(unique(results_df$athlete))),
       multiple = FALSE,
       options = list(placeholder = 'Начните вводить для поиска')
     )
@@ -121,7 +112,7 @@ ui <- fluidPage(
     selectizeInput(
       inputId = "ath_6",
       label = "Выберите спортсмена:",
-      choices = c("Оставить пустым", sort(unique(results_2025$athlete))),
+      choices = c("Оставить пустым", sort(unique(results_df$athlete))),
       multiple = FALSE,
       options = list(placeholder = 'Начните вводить для поиска')
     )
@@ -132,7 +123,7 @@ ui <- fluidPage(
     selectizeInput(
       inputId = "ath_7",
       label = "Выберите спортсмена:",
-      choices = c("Оставить пустым", sort(unique(results_2025$athlete))),
+      choices = c("Оставить пустым", sort(unique(results_df$athlete))),
       multiple = FALSE,
       options = list(placeholder = 'Начните вводить для поиска')
     )
@@ -143,7 +134,7 @@ ui <- fluidPage(
     selectizeInput(
       inputId = "ath_8",
       label = "Выберите спортсмена:",
-      choices = c("Оставить пустым", sort(unique(results_2025$athlete))),
+      choices = c("Оставить пустым", sort(unique(results_df$athlete))),
       multiple = FALSE,
       options = list(placeholder = 'Начните вводить для поиска')
     )
@@ -154,7 +145,7 @@ ui <- fluidPage(
     selectizeInput(
       inputId = "ath_9",
       label = "Выберите спортсмена:",
-      choices = c("Оставить пустым", sort(unique(results_2025$athlete))),
+      choices = c("Оставить пустым", sort(unique(results_df$athlete))),
       multiple = FALSE,
       options = list(placeholder = 'Начните вводить для поиска')
     )
@@ -187,15 +178,10 @@ server <- function(input, output) {
   ff <- reactive({
     req(input$comp)
 
-    results_2025 %>% 
+    results_df %>% 
       filter(Title == input$comp) %>% 
       ungroup()
-    # filter(split == "Финиш") 
-    # %>%
-    #   mutate(place = as.integer(as.character(place))) %>% 
-    #   filter(place < input$n_ath) 
-    
-    
+   
     
   })
   
@@ -210,24 +196,7 @@ server <- function(input, output) {
   })
   
   
-  splits <- reactive({
-    
-    find_laps(as.numeric(laps[laps$comp_id == input$comp, -(1:3)]))
-    
-  })
-  
-  # default_ath_vec <- reactive({
-  #   
-  #   ff() %>%
-  #     filter(split == unique(ff()$split)[length(unique(ff()$split))]) %>%
-  #     # filter(split == "Финиш") %>%
-  #     mutate(place = as.integer(place)) %>%
-  #     filter(place <= 6) %>%
-  #     pull(athlete) %>%
-  #     unique()
-  #   
-  # })
-  
+ 
   ath_vec <- reactive({
     req(input$ath_1)
     req(input$ath_2)
@@ -263,37 +232,9 @@ server <- function(input, output) {
                 mutate(ath_place = paste(athlete, place)) %>% 
                 select(bib, ath_place), by = "bib")
     
-    
-
-    
+        
   })
-  
-  
-  
-  # 
-  #   ff() %>%
-  #     # mutate(lap = cut(split_m, breaks = splits(), labels = 1:(length(splits) - 1))) %>%
-  #     filter(text_filter(athlete, ath_vec())) %>% 
-  #   # %>%
-  #     mutate(res_secs = as.numeric(ms(res))) %>%
-  #     group_by(athlete) %>%
-  #     mutate(lap_res = diff_w_next(res_secs)) %>%
-  #   #   ungroup() %>%
-  #   #   group_by(split) %>%
-  #   #   mutate(lap_best = min(lap_res, na.rm = T)) %>% #лучший показатель за отрезок запишем в столбец
-  #   #   mutate(diff_time = lap_res - lap_best,
-  #   #          speed = lap_len/lap_res) %>%
-  #   #   mutate(split = factor(split, levels = unique(ff$split)))
-  # 
-  # })
-  
-
-  # plot_1 <- reactive({
-  #   selected_data()  %>% 
-  #     ggplot(aes(x = split, y = diff_time, col = athlete, group = as.character(athlete))) + geom_point() + geom_line() +
-  #     theme_bw()
-  # })
-  
+    
   plot_1 <- reactive({
     selected_data() %>%
       ggplot(aes(x = split, y = diff_time, group = ath_place, col = ath_place))+
@@ -409,6 +350,7 @@ server <- function(input, output) {
 # Выполняем приложение 
 
 shinyApp(ui = ui, server = server)
+
 
 
 
